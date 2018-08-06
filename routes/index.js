a36689d18f4cc9767e0bcb8ba1c4afa0d4310656
Dave Cohen <dcohen@zagster.com>
2018-08-06 15:21:00 -0400

    Allrecipes update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { getRsmq } = require('../src/common');
+const router = express.Router();
+const RedisSMQ = require("rsmq");
+const allIndexStart = 39305;
+const allIndexEnd = 265000;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/allRecipes', async function(req, res, next) {
+  for (let i = allIndexStart; i <= allIndexEnd; i++) {
+    await getRsmq().sendMessage({ qname: 'allRecipes', message: i.toString() });
+  }
+  res.status(200).json({ status: 'Success' });
 });
 
 module.exports = router;
