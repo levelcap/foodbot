7088dac3f79150d64bdf69fed57d9b6dc297e8af
Dave Cohen <dcohen@zagster.com>
2018-08-06 15:29:47 -0400

    Dont wait before returning to avoid timeouts

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const allIndexEnd = 265000;
 /* GET home page. */
 router.get('/allRecipes', async function(req, res, next) {
   for (let i = allIndexStart; i <= allIndexEnd; i++) {
-    await getRsmq().sendMessage({ qname: 'allRecipes', message: i.toString() });
+    getRsmq().sendMessage({ qname: 'allRecipes', message: i.toString() });
   }
   res.status(200).json({ status: 'Success' });
 });
