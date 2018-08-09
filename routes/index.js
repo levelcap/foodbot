c4fecc9ad0d0057ad378ef6d5d46266eda78a09a
Dave Cohen <dcohen@zagster.com>
2018-08-09 09:58:47 -0400

    Better promise and error handling

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const _ = require('lodash');
 const { getRsmq } = require('../src/common');
 const router = express.Router();
-const allIndexStart = 117277;
+const allIndexStart = 211267;
 const allIndexEnd = 265000;
 const geniusStart = 40;
 const geniusEnd = 536702;
 
 /* GET home page. */
 router.get('/allRecipes', async function(req, res, next) {
+  const start = parseInt(_.get(req, 'query.start', '0'), 10);
+  const end = parseInt(_.get(req, 'query.end', '0'), 10);
+
   res.status(200).json({ status: 'Success' });
-  for (let i = allIndexStart; i <= allIndexEnd; i++) {
+  for (let i = start; i <= end; i++) {
     console.log(`Adding ${i} to queue`);
     try {
       await getRsmq().sendMessage({ qname: 'allRecipes', message: i.toString() });
@@ -21,8 +25,11 @@ router.get('/allRecipes', async function(req, res, next) {
 });
 
 router.get('/genius', async function(req, res, next) {
+  const start = parseInt(_.get(req, 'query.start', '0'), 10);
+  const end = parseInt(_.get(req, 'query.end', '0'), 10);
+
   res.status(200).json({ status: 'Success' });
-  for (let i = geniusStart; i <= geniusEnd; i++) {
+  for (let i = start; i <= end; i++) {
     console.log(`Adding ${i} to queue`);
     try {
       await getRsmq().sendMessage({ qname: 'allRecipes', message: i.toString() });
